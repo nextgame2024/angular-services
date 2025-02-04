5b66479b796cdc8523ae47e84a145669f59bdd37
nextgame2024 <nextgamebrisbane@gmail.com>
2025-02-04 21:36:37 +1000

    services

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { COURSES } from "./../db-data";
+import { CoursesService } from "./services/courses.service";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  InjectionToken,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from "@angular/core";
+import { Course } from "./model/course";
+import { CourseCardComponent } from "./course-card/course-card.component";
+import { HighlightedDirective } from "./directives/highlighted.directive";
+import { Observable } from "rxjs";
+import { HttpClient, HttpParams } from "@angular/common/http";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
+})
+export class AppComponent implements OnInit {
+  courses$: Observable<Course[]>;
+
+  constructor(private coursesService: CoursesService) {}
+
+  ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
+  }
+
+  save(course: Course) {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("Course Saved"));
+  }
+}
